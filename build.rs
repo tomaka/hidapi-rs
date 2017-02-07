34e64c84afdec9c417eb1636a34ef17b0e6c4352
Arkadiy Paronyan <arkadiy.paronyan@gmail.com>
2017-02-07 22:18:53 +0100

    Use hidraw on linux

--- build.rs
+++ build.rs
@@ -18,7 +18,6 @@
 // *************************************************************************
 
 extern crate gcc;
-extern crate pkg_config;
 
 fn main() {
     compile();
@@ -27,12 +26,9 @@ fn main() {
 #[cfg(target_os = "linux")]
 fn compile() {
     let mut config = gcc::Config::new();
-    config.file("etc/hidapi/libusb/hid.c").include("etc/hidapi/hidapi");
-    let lib = pkg_config::find_library("libusb-1.0").expect("Unable to find libusb-1.0");
-    for path in lib.include_paths {
-        config.include(path.to_str().expect("Failed to convert include path to str"));
-    }
+    config.file("etc/hidapi/linux/hid.c").include("etc/hidapi/hidapi");
     config.compile("libhidapi.a");
+    println!("cargo:rustc-link-lib=udev");
 }
 
 #[cfg(target_os = "windows")]
